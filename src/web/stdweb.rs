4106979847f2d183b815c609d4c0ad387af5c3c2
Ryan Goldstein <ryanisaacg@gmail.com>
2019-07-25 20:40:04 -0700

    Create a crate for cross-platform saving
    All of this code lived in Quicksilver, then quick-fs (name subject to
    change.) It is optional in Quicksilver and shares no code with quick-fs,
    which is mandatory.

---
+++ src/web/stdweb.rs
@@ -0,0 +1,23 @@
+use stdweb::web::window;
+use crate::SaveError;
+
+
+pub fn set_storage(is_local: bool, profile: &str, value: &str) -> Result<(), SaveError> {
+    let storage = if is_local {
+        window().local_storage()
+    } else {
+        window().session_storage()
+    };
+
+    storage.insert(profile, value).map_err(|_| SaveError::SaveWriteFailed)
+}
+
+pub fn get_storage(is_local: bool, profile: &str) -> Result<String, SaveError> {
+    let storage = if is_local {
+        window().local_storage()
+    } else {
+        window().session_storage()
+    };
+
+    storage.get(profile).ok_or_else(|| SaveError::SaveNotFound(profile.to_string()))
+}
